0b07b385bad40fca3f990f7d8e2aad2f1b425873
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-12 15:40:28 -0400

    feat: connect SearchParentFragment to children
    connect SearchParentFragment to child SearchFragment(s)

--- app/src/main/java/com/atmko/podcastapp/model/Genre.kt
+++ app/src/main/java/com/atmko/podcastapp/model/Genre.kt
@@ -2,4 +2,6 @@ package com.atmko.podcastapp.model
 
 import com.google.gson.annotations.SerializedName
 
+const val GENRE_ID_KEY = "genre"
+
 class Genre(@SerializedName("id") val id: Int)
--- app/src/main/java/com/atmko/podcastapp/view/SearchParentFragment.kt
+++ app/src/main/java/com/atmko/podcastapp/view/SearchParentFragment.kt
@@ -4,27 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.FragmentPagerAdapter
 import com.atmko.podcastapp.R
-import com.atmko.podcastapp.viewmodel.SearchViewModel
+import com.atmko.podcastapp.model.Genre
+import com.atmko.podcastapp.view.adapters.GenrePagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_search_parent.*
 
 class SearchParentFragment : Fragment() {
 
-    private lateinit var searchViewModel: SearchViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_search_parent, container, false)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        searchViewModel =
-            ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_search_parent, container, false)
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
-        return root
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        configureViews()
+    }
+
+    fun configureViews() {
+        tabLayout.removeAllTabs()
+        val genresNames: Array<String> = resources.getStringArray(R.array.genre_titles)
+        val genresIds: IntArray = resources.getIntArray(R.array.genre_ids)
+        val genres: MutableList<Genre> = mutableListOf()
+        for (i in genresNames.indices){
+            tabLayout.addTab(tabLayout.newTab().setText(genresNames[i]))
+            genres.add(Genre(genresIds[i]))
+        }
+
+        searchViewPager.apply {
+            offscreenPageLimit = 2
+            adapter = GenrePagerAdapter(childFragmentManager,
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, genres)
+            addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        }
+
+        tabLayout.apply {
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    tab?.let { searchViewPager.currentItem = tab.position  }
+                }
+            })
+        }
     }
 }
