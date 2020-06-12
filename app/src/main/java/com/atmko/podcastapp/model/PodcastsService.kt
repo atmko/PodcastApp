3f7229891149daf6cf2f81c07198f270a7e1c04a
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-12 15:57:06 -0400

    feat: implement PodcastsApi interface
    create PodcastsService to implement PodcastsApi methods

---
+++ app/src/main/java/com/atmko/podcastapp/model/PodcastsService.kt
@@ -0,0 +1,24 @@
+package com.atmko.podcastapp.model
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PodcastsService : PodcastsApi {
+    private val BASE_URL = "https://listen-api.listennotes.com/api/v2/"
+    private val api: PodcastsApi
+
+    init {
+        api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(PodcastsApi::class.java)
+    }
+
+    override fun getPodcastsByGenre(genreId: Int): Single<ApiResults> {
+        return api.getPodcastsByGenre(genreId)
+    }
+}
