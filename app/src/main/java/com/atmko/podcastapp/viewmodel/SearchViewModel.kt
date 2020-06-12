6cd2012a9650d03aa629f2dab413aae84772337b
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-12 16:00:20 -0400

    feat: call api via SearchViewModel

--- app/src/main/java/com/atmko/podcastapp/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/atmko/podcastapp/viewmodel/SearchViewModel.kt
@@ -1,13 +1,49 @@
 package com.atmko.podcastapp.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.atmko.podcastapp.model.ApiResults
+import com.atmko.podcastapp.model.Podcast
+import com.atmko.podcastapp.model.PodcastsService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
-class SearchViewModel : ViewModel() {
+class SearchViewModel: ViewModel() {
+    val searchResults:MutableLiveData<List<Podcast>> = MutableLiveData()
+    val loadError: MutableLiveData<Boolean> = MutableLiveData()
+    val loading: MutableLiveData<Boolean> = MutableLiveData()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is search Fragment"
+    private val podcastService: PodcastsService = PodcastsService()
+    private val disposable: CompositeDisposable = CompositeDisposable()
+
+    fun refresh(genreId: Int) {
+        fetchPodcasts(genreId)
     }
-    val text: LiveData<String> = _text
+
+    fun fetchPodcasts(genreId: Int) {
+        loading.value = true
+        disposable.add(
+            podcastService.getPodcastsByGenre(genreId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<ApiResults>() {
+                    override fun onSuccess(results: ApiResults?) {
+                        searchResults.value = results?.podcasts
+                        loadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        loadError.value = true
+                        loading.value = false
+                    }
+                })
+        )
+    }
+
+    override fun onCleared() {
+        disposable.clear()
+    }
 }
