4001b49470b0a4e9c0e23aa03cb60d0a6c284dac
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-13 10:17:00 -0400

    refactor: remove unneeded gson SerializedName

--- app/src/main/java/com/atmko/podcastapp/model/Genre.kt
+++ app/src/main/java/com/atmko/podcastapp/model/Genre.kt
@@ -1,10 +1,6 @@
 package com.atmko.podcastapp.model
 
-import com.google.gson.annotations.SerializedName
-
 const val GENRE_ID_KEY = "genre_id"
 const val GENRE_NAME_KEY = "genre_name"
 
-class Genre(
-    @SerializedName("id") val id: Int,
-    @SerializedName("name") val name: String)
+class Genre(val id: Int, val name: String)
