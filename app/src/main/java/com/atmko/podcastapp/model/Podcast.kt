d7027e79c8327d98fa7f33ba95c353bb195ebe50
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-14 15:17:11 -0400

    feat: create secondary constructor for Podcast
    create secondary constructor for Podcast details that takes in episodes

--- app/src/main/java/com/atmko/podcastapp/model/Podcast.kt
+++ app/src/main/java/com/atmko/podcastapp/model/Podcast.kt
@@ -8,4 +8,12 @@ class Podcast(@SerializedName("id") val id: String,
               val image: String,
               val description: String,
               @SerializedName("total_episodes") val totalEpisodes: Int) {
+
+    var episodes: List<Episode> = mutableListOf()
+
+    constructor(id: String, title: String, publisher: String, image: String, description: String,
+                totalEpisodes: Int, episodes: List<Episode>):
+            this(id, title, publisher, image, description, totalEpisodes) {
+        this.episodes = episodes
+    }
 }
