eeb9dd00afab29483513dbecd8e99b231b569d84
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-06-16 10:12:31 -0400

    feat: add PodcastService injection method
    create PodcastService injection method for EpisodeViewModel

--- app/src/main/java/com/atmko/podcastapp/dependencyinjection/ApiComponent.kt
+++ app/src/main/java/com/atmko/podcastapp/dependencyinjection/ApiComponent.kt
@@ -2,6 +2,7 @@ package com.atmko.podcastapp.dependencyinjection
 
 import com.atmko.podcastapp.model.PodcastsService
 import com.atmko.podcastapp.viewmodel.DetailsViewModel
+import com.atmko.podcastapp.viewmodel.EpisodeViewModel
 import com.atmko.podcastapp.viewmodel.SearchViewModel
 import dagger.Component
 
@@ -10,4 +11,5 @@ interface ApiComponent {
     fun inject(service: PodcastsService)
     fun inject(service: SearchViewModel)
     fun inject(service: DetailsViewModel)
+    fun inject(service: EpisodeViewModel)
 }
