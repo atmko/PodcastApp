d4af691329646cd3c05a35840028ffe61310b970
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-13 15:13:05 -0400

    feat: create User class

---
+++ app/src/main/java/com/atmko/skiptoit/model/User.kt
@@ -0,0 +1,12 @@
+package com.atmko.skiptoit.model
+
+import com.google.gson.annotations.SerializedName
+
+class User(@SerializedName("user_id")
+           val userId: String,
+           @SerializedName("google_id")
+           val googleId: String,
+           @SerializedName("username")
+           val username: String,
+           @SerializedName("profile_image")
+           val profileImage: String)
