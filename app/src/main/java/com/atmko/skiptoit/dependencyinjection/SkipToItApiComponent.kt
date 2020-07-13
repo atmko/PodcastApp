8b70abbc580d946efed6df7336df36b1de4385f3
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-13 16:23:31 -0400

    feat: add class dependencies for MasterActivityViewModel

--- app/src/main/java/com/atmko/skiptoit/dependencyinjection/SkipToItApiComponent.kt
+++ app/src/main/java/com/atmko/skiptoit/dependencyinjection/SkipToItApiComponent.kt
@@ -2,10 +2,12 @@ package com.atmko.skiptoit.dependencyinjection
 
 import com.atmko.skiptoit.model.SkipToItService
 import com.atmko.skiptoit.viewmodel.CommentsViewModel
+import com.atmko.skiptoit.viewmodel.MasterActivityViewModel
 import dagger.Component
 
 @Component(modules = [SkipToItApiModule::class])
 interface SkipToItApiComponent {
     fun inject(service: SkipToItService)
     fun inject(service: CommentsViewModel)
+    fun inject(service: MasterActivityViewModel)
 }
