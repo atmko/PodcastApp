08f15d5792ff5bb7fdfaf24372e531790982df46
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-14 19:07:42 -0400

    feat: create secondary constructer for Comment
    add secondary constructer for Comment to be posted to server

--- app/src/main/java/com/atmko/skiptoit/model/Comment.kt
+++ app/src/main/java/com/atmko/skiptoit/model/Comment.kt
@@ -4,8 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 class Comment(
     @SerializedName("comment_id")
-    val commentId: String,
+    val commentId: String?,
     val username: String,
     val body: String,
     @SerializedName("profile_image")
-    val profileImage: String?)
+    val profileImage: String?) {
+
+    constructor(username: String, body: String):
+            this(null, username, body, null) {
+    }
+}
