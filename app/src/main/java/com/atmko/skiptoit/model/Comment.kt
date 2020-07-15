9c58895b7da9090ecca1363fc7a7c24880b824d9
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-14 20:07:08 -0400

    feat: allow null Comment username values
    make Comment username nullable. username will be provied on the server

--- app/src/main/java/com/atmko/skiptoit/model/Comment.kt
+++ app/src/main/java/com/atmko/skiptoit/model/Comment.kt
@@ -5,12 +5,12 @@ import com.google.gson.annotations.SerializedName
 class Comment(
     @SerializedName("comment_id")
     val commentId: String?,
-    val username: String,
+    val username: String?,
     val body: String,
     @SerializedName("profile_image")
     val profileImage: String?) {
 
-    constructor(username: String, body: String):
-            this(null, username, body, null) {
+    constructor(body: String):
+            this(null, null, body, null) {
     }
 }
