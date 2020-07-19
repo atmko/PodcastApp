eda5adbf296a7acffc42404ca3f2bc26ed9a85b4
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-19 14:05:52 -0400

    feat: use view binding in SubscriptionsFragment

--- app/src/main/java/com/atmko/skiptoit/view/SubscriptionsFragment.kt
+++ app/src/main/java/com/atmko/skiptoit/view/SubscriptionsFragment.kt
@@ -4,29 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.atmko.skiptoit.R
+import com.atmko.skiptoit.databinding.FragmentSubscriptionsBinding
 import com.atmko.skiptoit.viewmodel.SubscriptionsViewModel
 
 class SubscriptionsFragment : Fragment() {
+    private var _binding: FragmentSubscriptionsBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var subscriptionsViewModel: SubscriptionsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        subscriptionsViewModel =
-            ViewModelProviders.of(this).get(SubscriptionsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_subscriptions, container, false)
-        val textView: TextView = root.findViewById(R.id.subscriptionsText)
-        subscriptionsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentSubscriptionsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
