5a50502476e9639561bccca9c1ee3b4b827013bf
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-19 19:27:00 -0400

    feat: implement PodcastAdapter and navigate
    implement PodcastAdapter in SubscriptionsFragment and navigate to
    DetailsFragment when podcast item clicked

--- app/src/main/java/com/atmko/skiptoit/view/SubscriptionsFragment.kt
+++ app/src/main/java/com/atmko/skiptoit/view/SubscriptionsFragment.kt
@@ -5,19 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.atmko.skiptoit.R
 import com.atmko.skiptoit.databinding.FragmentSubscriptionsBinding
+import com.atmko.skiptoit.model.Podcast
+import com.atmko.skiptoit.view.adapters.PodcastAdapter
 import com.atmko.skiptoit.viewmodel.SubscriptionsViewModel
 
-class SubscriptionsFragment : Fragment() {
+class SubscriptionsFragment : Fragment(), PodcastAdapter.OnPodcastItemClickListener {
     private var _binding: FragmentSubscriptionsBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var subscriptionsViewModel: SubscriptionsViewModel
+    private val podcastAdapter: PodcastAdapter =
+        PodcastAdapter(arrayListOf(), R.layout.item_podcast_list, this)
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding = FragmentSubscriptionsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        configureViews()
+    }
+
+    private fun configureViews() {
+        binding.resultsFrameLayout.resultsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = podcastAdapter
+        }
+    }
+
+    override fun onItemClick(podcast: Podcast) {
+        val action = SubscriptionsFragmentDirections
+                .actionNavigationSubscriptionsToNavigationDetails(podcast.id)
+        view?.findNavController()?.navigate(action)
     }
 }
