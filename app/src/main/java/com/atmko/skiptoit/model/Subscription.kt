e9038c94d381ea2f0e02b4b6e028496e32a9f6ee
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-19 12:03:52 -0400

    feat: create Subscription model class

---
+++ app/src/main/java/com/atmko/skiptoit/model/Subscription.kt
@@ -0,0 +1,7 @@
+package com.atmko.skiptoit.model
+
+import com.google.gson.annotations.SerializedName
+
+class Subscription(@SerializedName("listen_notes_id")
+                   val listenNotesId: String,
+                   val image: String?)
