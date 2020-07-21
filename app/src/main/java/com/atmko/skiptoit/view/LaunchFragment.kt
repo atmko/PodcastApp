6e6f0d1bdac2aaab397a55a9cfa7d55a6b930fb5
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-21 09:22:17 -0400

    feat: attempt to install orivider in LaunchFragment

--- app/src/main/java/com/atmko/skiptoit/view/LaunchFragment.kt
+++ app/src/main/java/com/atmko/skiptoit/view/LaunchFragment.kt
@@ -11,16 +11,21 @@ import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.atmko.skiptoit.R
 import com.atmko.skiptoit.databinding.FragmentLaunchBinding
 import com.atmko.skiptoit.viewmodel.MasterActivityViewModel
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.security.ProviderInstaller
 import com.google.android.material.snackbar.Snackbar
 
 const val LAUNCH_FRAGMENT_KEY = "launch_fragment"
 const val IS_FIRST_SETUP_KEY = "is_first_set_up"
+private const val ERROR_DIALOG_REQUEST_CODE = 1
 
 class LaunchFragment : Fragment(),
+    ProviderInstaller.ProviderInstallListener,
     MasterActivityViewModel.ViewNavigation {
 
     private var _binding: FragmentLaunchBinding? = null
@@ -37,11 +42,36 @@ class LaunchFragment : Fragment(),
 
         configureViews()
         configureValues()
+        retryProviderInstall()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
+        val availability = GoogleApiAvailability.getInstance()
+        if (availability.isUserResolvableError(errorCode)) {
+            // Recoverable error. Show a dialog prompting the user to
+            // install/update/enable Google Play services.
+            activity?.let {
+                availability.showErrorDialogFragment(
+                    it,
+                    errorCode,
+                    ERROR_DIALOG_REQUEST_CODE
+                ) {
+                    // The user chose not to take the recovery action
+                    onProviderInstallerNotAvailable()
+                }
+            }
+        } else { // Google Play services is not available.
+            onProviderInstallerNotAvailable()
+        }
+    }
+
+    override fun onProviderInstalled() {
+
     }
 
     private fun configureViews() {
@@ -91,6 +121,12 @@ class LaunchFragment : Fragment(),
     private fun configureValues() {
     }
 
+    private fun retryProviderInstall() {
+        context?.let {
+            ProviderInstaller.installIfNeededAsync(it, this)
+        }
+    }
+
     private fun launchBrowserIntent(url: String) {
         val webPage: Uri = Uri.parse(url)
         val intent = Intent(Intent.ACTION_VIEW, webPage)
@@ -101,6 +137,15 @@ class LaunchFragment : Fragment(),
                 Snackbar.make(binding.topLayout,
                     R.string.no_browser_error_message, Snackbar.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun onProviderInstallerNotAvailable() {
+        context?.let {
+            Toast.makeText(
+                it, getString(R.string.couldnt_update_google_play_services),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
