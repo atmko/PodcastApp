483b469eb40319489fc5d764b8318bb4bc078e4a
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-24 11:08:54 -0400

    feat: add toggleSubscription to DetailsViewModel
    ad method to DetailsViewModel to change subscription status

--- app/src/main/java/com/atmko/skiptoit/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/atmko/skiptoit/viewmodel/DetailsViewModel.kt
@@ -1,16 +1,20 @@
 package com.atmko.skiptoit.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.atmko.skiptoit.dependencyinjection.DaggerListenNotesApiComponent
 import com.atmko.skiptoit.model.Podcast
 import com.atmko.skiptoit.model.PodcastsApi
+import com.atmko.skiptoit.model.SkipToItApi
 import com.atmko.skiptoit.model.SkipToItDatabase
+import com.atmko.skiptoit.util.AppExecutors
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import retrofit2.Response
 import javax.inject.Inject
 
 class DetailsViewModel(private val googleSignInAccount: GoogleSignInAccount?,
@@ -23,10 +27,19 @@ class DetailsViewModel(private val googleSignInAccount: GoogleSignInAccount?,
 
     @Inject
     lateinit var podcastService: PodcastsApi
+    @Inject
+    lateinit var skipToItService: SkipToItApi
     private val disposable: CompositeDisposable = CompositeDisposable()
+
+    val isSubscribed: LiveData<Boolean>
+    val processError: MutableLiveData<Boolean> = MutableLiveData()
+    val processing: MutableLiveData<Boolean> = MutableLiveData()
 
     init {
         DaggerListenNotesApiComponent.create().inject(this)
+        isSubscribed = skipToItDatabase.subscriptionsDao().isSubscribed(podcast.id)
+        processError.value = false
+        processing.value = false
     }
 
     fun refresh(podcastId: String) {
@@ -48,6 +61,53 @@ class DetailsViewModel(private val googleSignInAccount: GoogleSignInAccount?,
                     }
                 })
         )
+    }
+    fun toggleSubscription() {
+        toggleRemoteSubscriptionStatus()
+    }
+
+    private fun toggleRemoteSubscriptionStatus() {
+        val subscribeStatus = if (isSubscribed.value!!) STATUS_UNSUBSCRIBE else STATUS_SUBSCRIBE
+        googleSignInAccount?.let { account ->
+            account.idToken?.let {
+                processing.value = true
+                disposable.add(
+                    skipToItService.subscribeOrUnsubscribe(podcast.id, it, subscribeStatus)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(object : DisposableSingleObserver<Response<Void>>() {
+                            override fun onSuccess(response: Response<Void>) {
+                                if (response.isSuccessful) {
+                                    toggleLocalSubscriptionStatus(subscribeStatus)
+                                } else {
+                                    processError.value = true
+                                    processing.value = false
+                                }
+                            }
+
+                            override fun onError(e: Throwable?) {
+                                processError.value = true
+                                processing.value = false
+                            }
+                        })
+                )
+            }
+        }
+    }
+
+    private fun toggleLocalSubscriptionStatus(subscribeStatus: Int) {
+        AppExecutors.getInstance().diskIO().execute(Runnable {
+            if (subscribeStatus == STATUS_SUBSCRIBE) {
+                skipToItDatabase.subscriptionsDao().createSubscription(podcast)
+            } else {
+                skipToItDatabase.subscriptionsDao().deleteSubscription(podcast)
+            }
+
+            AppExecutors.getInstance().mainThread().execute(Runnable {
+                processError.value = false
+                processing.value = false
+            })
+        })
     }
 
     override fun onCleared() {
