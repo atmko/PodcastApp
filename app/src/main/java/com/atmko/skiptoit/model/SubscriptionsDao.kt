c4bd17647fa4cfe2956dd5d1810ae29d843a03f2
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-24 07:26:49 -0400

    feat: add isSubscribed query for SubscriptionsDao

--- app/src/main/java/com/atmko/skiptoit/model/SubscriptionsDao.kt
+++ app/src/main/java/com/atmko/skiptoit/model/SubscriptionsDao.kt
@@ -8,6 +8,9 @@ interface SubscriptionsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun createSubscription(podcast: Podcast)
 
+    @Query("SELECT EXISTS (SELECT 1 FROM subscriptions WHERE id = :podcastId)")
+    fun isSubscribed(podcastId: String): LiveData<Boolean>
+
     @Query("SELECT * FROM subscriptions")
     fun getAllSubscriptions(): LiveData<List<Podcast>>
 
