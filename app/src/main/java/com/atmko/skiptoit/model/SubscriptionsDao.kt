22ed9bad8ee127b67275cc3eddabbbdc1087d103
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-24 06:40:08 -0400

    fix: error caused by not using live data

--- app/src/main/java/com/atmko/skiptoit/model/SubscriptionsDao.kt
+++ app/src/main/java/com/atmko/skiptoit/model/SubscriptionsDao.kt
@@ -1,5 +1,6 @@
 package com.atmko.skiptoit.model
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
@@ -8,7 +9,7 @@ interface SubscriptionsDao {
     fun createSubscription(podcast: Podcast)
 
     @Query("SELECT * FROM subscriptions")
-    fun getAllSubscriptions(): List<Podcast>
+    fun getAllSubscriptions(): LiveData<List<Podcast>>
 
     @Delete
     fun deleteSubscription(podcast: Podcast)
