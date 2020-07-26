dbdef1d0ffe8dcbc425477f2d3bea8331d602fef
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-07-26 18:51:56 -0400

    fix: stop fragment reload on nav item selected again
    stop reloadinfg fragment when navigation item is currently selected
    and loaded
    solution - overinding OnNavigationItemReselectedListener

--- app/src/main/java/com/atmko/skiptoit/view/MasterActivity.kt
+++ app/src/main/java/com/atmko/skiptoit/view/MasterActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
+import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
@@ -236,6 +237,15 @@ class MasterActivity : AppCompatActivity(), MasterActivityViewModel.ViewNavigati
             )
         )
         navView.setupWithNavController(navController)
+
+        navView.setOnNavigationItemReselectedListener(
+            object : BottomNavigationView.OnNavigationItemReselectedListener {
+            override fun onNavigationItemReselected(item: MenuItem) {
+                if (navView.selectedItemId != item.itemId) {
+                    onNavigationItemReselected(item)
+                }
+            }
+        })
     }
 
     fun signIn() {
