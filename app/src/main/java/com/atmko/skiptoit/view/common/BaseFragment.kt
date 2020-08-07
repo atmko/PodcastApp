de30438be92a4826993e01b87989b744901ad208
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-08-07 16:10:48 -0400

    feat: create BaseFragment class

---
+++ app/src/main/java/com/atmko/skiptoit/view/common/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.atmko.skiptoit.view.common
+
+import androidx.annotation.UiThread
+import androidx.fragment.app.Fragment
+import com.atmko.skiptoit.SkipToItApplication
+import com.atmko.skiptoit.dependencyinjection.application.ApplicationComponent
+import com.atmko.skiptoit.dependencyinjection.presentation.PresentationComponent
+import com.atmko.skiptoit.dependencyinjection.presentation.PresentationModule
+
+open class BaseFragment : Fragment() {
+
+    private var isInjected: Boolean = false
+
+    @UiThread
+    protected fun getPresentationComponent(): PresentationComponent {
+        if (!isInjected) {
+            isInjected = true
+            return getApplicationComponent()
+                    .newPresentationComponent(PresentationModule())
+        }
+
+        throw RuntimeException("getPresentationComponent() called more than once")
+    }
+
+    private fun getApplicationComponent(): ApplicationComponent {
+        return (requireActivity().application as SkipToItApplication).appComponent
+    }
+}
