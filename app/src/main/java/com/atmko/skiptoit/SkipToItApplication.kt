03cea2e210cdcaf274e78e1dd4b232f039857fc6
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-08-09 08:06:55 -0400

    fix: no playback notification
    cause - not creating notification channel before starting playback
    notification
    
    solution - create notification on application start

--- app/src/main/java/com/atmko/skiptoit/SkipToItApplication.kt
+++ app/src/main/java/com/atmko/skiptoit/SkipToItApplication.kt
@@ -1,9 +1,13 @@
 package com.atmko.skiptoit
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.atmko.skiptoit.dependencyinjection.application.ApplicationComponent
 import com.atmko.skiptoit.dependencyinjection.application.ApplicationModule
 import com.atmko.skiptoit.dependencyinjection.application.DaggerApplicationComponent
+import com.atmko.skiptoit.services.NOTIFICATION_CHANNEL_ID_PLAYBACK
 
 class SkipToItApplication : Application() {
 
@@ -16,7 +20,24 @@ class SkipToItApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        appComponent.inject(this);
+        createNotificationChannel()
+        appComponent.inject(this)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel
+            val name = getString(R.string.notification_channel_playback)
+            val descriptionText = getString(R.string.channel_description_playback)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel =
+                NotificationChannel(NOTIFICATION_CHANNEL_ID_PLAYBACK, name, importance)
+            notificationChannel.description = descriptionText
+            notificationChannel.setSound(null,null)
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 
 }
