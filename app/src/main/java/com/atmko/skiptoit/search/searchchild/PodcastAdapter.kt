d5d6a744e49a5d1fae14c66ecfb0f7f26c2340b4
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-10-25 07:53:51 -0400

    refactor: replace synthetic binding in PodcastAdapter

--- app/src/main/java/com/atmko/skiptoit/search/searchchild/PodcastAdapter.kt
+++ app/src/main/java/com/atmko/skiptoit/search/searchchild/PodcastAdapter.kt
@@ -3,16 +3,11 @@ package com.atmko.skiptoit.search.searchchild
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.atmko.skiptoit.R
+import com.atmko.skiptoit.databinding.ItemPodcastListBinding
 import com.atmko.skiptoit.model.Podcast
 import com.atmko.skiptoit.utils.loadNetworkImage
-import kotlinx.android.synthetic.main.item_podcast_list.view.*
-import kotlinx.android.synthetic.main.item_podcast_square.view.podcastImageView
 
 class PodcastAdapter(private val clickListener: OnPodcastItemClickListener) :
     PagedListAdapter<Podcast, PodcastAdapter.PodcastViewHolder>(Podcast.PodcastDiffCallback()) {
@@ -23,19 +18,16 @@ class PodcastAdapter(private val clickListener: OnPodcastItemClickListener) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PodcastViewHolder {
-        val view: View = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.item_podcast_list, parent, false)
-        return PodcastViewHolder(view)
+        return PodcastViewHolder(
+            ItemPodcastListBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
+        )
     }
 
-    inner class PodcastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    inner class PodcastViewHolder(var binding: ItemPodcastListBinding) : RecyclerView.ViewHolder(binding.root),
         View.OnClickListener {
-        val podcastImageView: ImageView = itemView.podcastImageView
-        val title: TextView = itemView.title
-        val publisher: TextView = itemView.publisher
-        val totalEpisodes: TextView = itemView.totalEpisodes
-        val toggleSubscriptionButton: ImageButton = itemView.toggleSubscriptionButton
 
         init {
             itemView.setOnClickListener(this)
@@ -48,12 +40,12 @@ class PodcastAdapter(private val clickListener: OnPodcastItemClickListener) :
 
     override fun onBindViewHolder(holder: PodcastViewHolder, position: Int) {
         val podcast: Podcast = getItem(holder.adapterPosition)!!
-        holder.title.text = podcast.title
-        holder.publisher.text = podcast.publisher
-        holder.podcastImageView.loadNetworkImage(podcast.image)
-        holder.totalEpisodes.text =
-            String.format(holder.totalEpisodes.text.toString(), podcast.totalEpisodes)
-        holder.toggleSubscriptionButton.setOnClickListener {
+        holder.binding.title.text = podcast.title
+        holder.binding.publisher.text = podcast.publisher
+        holder.binding.podcastImageView.loadNetworkImage(podcast.image)
+        holder.binding.totalEpisodes.text =
+            String.format(holder.binding.totalEpisodes.text.toString(), podcast.totalEpisodes)
+        holder.binding.toggleSubscriptionButton.setOnClickListener {
             clickListener.onSubscriptionToggle(podcast)
         }
     }
